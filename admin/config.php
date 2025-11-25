d09066c8d039b1cd1f39593a0b8ca232b2e42e5d
penkisrinivasrao43 <penkisrinivasarao43@gmail.com>
2025-11-25 11:26:46 +0530

    database file change

--- admin/config.php
+++ admin/config.php
@@ -1,14 +1,11 @@
 <?php
 $servername = "localhost:3307";
 $username   = "root";
-$password   = "";  // use empty string if phpMyAdmin doesn’t ask for a password
-$dbname     = "wwwsldnadaswaram_sldnadaswaram"; // make sure this DB exists
-
+$password   = "";  
+$dbname     = "wwwsldnadaswaram_sldnadaswaram";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
 ?>
